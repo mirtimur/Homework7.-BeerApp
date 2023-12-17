51c5dbe429cd14e5363937277c1861bd1cd2d78a
Mir TImur <mirtimur1@gmail.com>
2023-12-17 13:34:31 +0300

    Homework

---
+++ TMSHomework7BeerApp/TMSHomework7BeerApp/BeerPaulaner.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+class BeerPaulaner{
+    
+    static let shared = BeerPaulaner()
+    
+    var liters = 20
+    var cost = 8
+}
---
+++ TMSHomework7BeerApp/TMSHomework7BeerApp/BeerStellaArtois.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+class BeerStellaArtois{
+    
+    static let shared = BeerStellaArtois()
+    
+    var liters = 20
+    var cost = 4
+}
